f90f1d1518c0579cf9b24fb512fdd5cdd5c81e34
GunhoKim <kgh3680@gmail.com>
2022-01-09 14:55:30 +0900

    Add Dto & Test Code

--- src/main/java/com/gunho/book/springboot/web/HelloController.java
+++ src/main/java/com/gunho/book/springboot/web/HelloController.java
@@ -1,14 +1,23 @@
 package com.gunho.book.springboot.web;
 
+import com.gunho.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
+    public String hello()
+    {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto (@RequestParam("name") String name, @RequestParam("amount") int amount)
+    {
+        return new HelloResponseDto(name, amount);
     }
 
 }
